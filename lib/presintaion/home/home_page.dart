9f5ddcde11b385476dba7c4b41b0d82a630d814e
Muhammadsharif <muhammadsharifbozorov1005@gmail.com>
2023-08-07 06:37:40 -0700

    bloc example

---
+++ lib/presintaion/home/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:bloc_example/presintaion/home/bloc/home_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Home page'),
+          ),
+        );
+      },
+    );
+  }
+}
